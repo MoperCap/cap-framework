2e56e37487ee90056eb5c5b35fd039323a9252d0
moper <2336246702@qq.com>
2026-02-24 10:15:31 +0800

    实现了@Bean功能

---
+++ cap-context/src/main/java/org/moper/cap/context/annotation/Bean.java
@@ -0,0 +1,10 @@
+package org.moper.cap.context.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Bean {
+    String name() default "";
+}
