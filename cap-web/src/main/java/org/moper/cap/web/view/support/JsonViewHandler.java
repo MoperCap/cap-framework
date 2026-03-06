c12ae1f886c803f56b39dda9c2aef9a90091ead0
moper <2336246702@qq.com>
2026-03-06 22:19:14 +0800

    修复了JsonView无法序列化Java8 Date的bug

--- cap-web/src/main/java/org/moper/cap/web/view/support/JsonViewHandler.java
+++ cap-web/src/main/java/org/moper/cap/web/view/support/JsonViewHandler.java
@@ -1,6 +1,8 @@
 package org.moper.cap.web.view.support;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.moper.cap.web.view.ViewHandler;
 import org.moper.cap.web.view.ResponseEntity;
 import org.moper.cap.web.router.RouteDefinition;
@@ -21,6 +23,10 @@ public class JsonViewHandler implements ViewHandler {
 
     public JsonViewHandler() {
         this.objectMapper = new ObjectMapper();
+        // 注册JSR310模块支持Java8时间类型
+        objectMapper.registerModule(new JavaTimeModule());
+        // 避免LocalDateTime默认序列化为时间戳
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 
     @Override
